75e42894d8fb4c2978966d80b867e6b426969606
Parivesh2005 <108748552+Parivesh2005@users.noreply.github.com>
2022-07-16 15:42:38 -0600

    Added codes that apply to the function

--- main.js
+++ main.js
@@ -50,47 +50,77 @@ function mainMenu() {
 // ******************************************************
 function firstTo40() {
   // Set the grade of the first student to 40.
+  grades[0] = 40;
   outputEl.innerHTML = "First grade to 40";
 }
 
 function lastTo50() {
   // Set the grade of the last student to 50.
+  grades[grades.length - 1] = 50;
   outputEl.innerHTML = "Last grade to 50";
 }
 
 function randomTo100() {
   // Set the grade of a random student to 100.
+  grades[randomInt(0, grades.length)] = maxGrade;
   outputEl.innerHTML = "Random grade to 100";
 }
 
 function addRandomGrade() {
   // Add a random grade between 0 and 100 to the end of the array.
+  grades.splice(grades.length, 0, Math.random() * 100);
   outputEl.innerHTML = "Add random grade";
 }
 
 function removeLastGrade() {
   // Remove the last grade.
+  grades.pop();
   outputEl.innerHTML = "Remove the last grade";
 }
 
 function countBelow50() {
   // Count how many grades are below 50.  Output the result.
-  outputEl.innerHTML = "Count grades below 50";
-}
+  
+  let count = 0
+  for(let i = 0; i < grades.length; i++){
+    if(grades[i] < 50){
+     count ++
+    }
+  }
+    outputEl.innerHTML = count + " Students have grades below 50";
+  }
 
 function lowGradesTo50() {
   // Change all grades that are below 50 to be equal to 50.
+  for(let i = 0; i < grades.length; i++){
+    if(grades[i] < 50){
+    grades[i] = 50;}
   outputEl.innerHTML = "Change low grades to 50";
 }
-
+}
 function increaseGradesBy10() {
   // Increase each grade by 10%.
+  for(let i = 0; i < grades.length; i++){
+    if (grades[i] <= 90) {
+    grades[i] = grades[i] + 10;
+    }
+    else if (grades[i] < 100){
+      grades[i] = maxGrade;
+    }
   outputEl.innerHTML = "Increase all grades by 10%";
 }
-
+}
 function decreaseGradesBy10() {
   // Decrease each grade by 10%.
+  for(let i = 0; i < grades.length; i++){
+    if (grades[i] >= 10) {
+      grades[i] = grades[i] - 10;
+      }
+      else if (grades[i] > 0){
+        grades[i] = 0;
+      }
   outputEl.innerHTML = "Decrease all grades by 10%";
+}
 }
 
 
@@ -103,4 +133,8 @@ function drawArray() {
     outputStr += `<div style="height:${divHeight}px"></div>`;
   }
   containerEl.innerHTML = outputStr;
+}
+// Function that returns random integer
+function randomInt(low, high){
+  return Math.floor(Math.random() * (high - low) + low);
 }
